e0bd22fc5edf584a94fbb4416d7d479ad2f07766
Shenedia Ellington <artistic.nita@gmail.com>
2024-01-22 13:49:48 -0500

    group exercise day1

---
+++ exercise.py
@@ -0,0 +1,37 @@
+#Exercise 1
+user_text = input("Enter some text: ")
+print(user_text)
+
+#Excercise 2
+user_number = input("enter a number: ")
+user_int = int(user_number)
+print(user_int+1)
+
+#Exercise 3
+user_float = input("enter a number: ")
+string_to_float = float(user_float)
+print(string_to_float +.5)
+
+#Exercise 4
+float_1 = input("enter a number: ")
+float_2 = input("enter another number: ")
+num1 = float(float_1)
+num2 = float(float_2)
+total = num1+num2
+print(total)
+
+#Exercise 5
+float_3 = input("enter a number: ")
+float_4 = input("enter another number: ")
+num3 = float(float_3)
+num4 = float(float_4)
+total_mult = num3*num4
+print(total_mult)
+
+#Exercise 6
+int_1 = input("enter a number: ")
+int_2 = input("enter another number: ")
+num_5 = int(int_1)
+num_6 = int(int_2)
+total_div = num_5/num_6
+print(total_div)
